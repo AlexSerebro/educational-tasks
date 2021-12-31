f5de632c08190453519dc3a5f6148ef05c07e0ab
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2021-12-31 12:13:04 +0200

    add task

---
+++ quarterofyear.js
@@ -0,0 +1,11 @@
+const quarterOf = (month) => {
+  return month <= 3
+    ? 1
+    : month > 3 && month <= 6
+    ? 2
+    : month > 6 && month <= 9
+    ? 3
+    : 4;
+};
+
+console.log(quarterOf(3));
